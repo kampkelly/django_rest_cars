864abc161459a57d7989655805458b1ddba4c6a6
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-14 15:23:51 +0100

    ft(retrieve-update-destroy-car): retrieve, update and destroy a car
    - retrieve car
    - update car
    - destroy a car

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ListCreateCar
+from .views import ListCreateCar, RetrieveUpdateDestroyCar
 
 app_name = 'cars'
 
 urlpatterns = {
-    url(r'^cars/$', ListCreateCar.as_view(), name="create")
+    url(r'^cars/$', ListCreateCar.as_view(), name="create"),
+    url(r'^cars/(?P<pk>[0-9]+)/$', RetrieveUpdateDestroyCar.as_view(), name="details")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
