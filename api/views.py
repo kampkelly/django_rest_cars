f2e38aa5162b6b46bc55c8bf42082dac3ba13a47
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-14 17:17:06 +0100

    ft(filter-cars): filter cars by query parameters
    - filter cars
    - refactor create view

--- api/views.py
+++ api/views.py
@@ -4,7 +4,25 @@ from rest_framework import generics
 from .serializers import CarSerializer
 from cars.models import Car
 
-class ListCreateCar(generics.ListCreateAPIView):
+
+class ListCar(generics.ListAPIView):
+    """This class defines the list behavior of our rest api."""
+    serializer_class = CarSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the cars (filtered
+        by query parameters passed)
+        """
+        queryset = Car.objects.all()
+        car_name = self.request.query_params.get('name', None)
+        params = [(param, self.request.query_params.get(param, None)) for param in self.request.query_params if self.request.query_params.get(param) != None]
+        if params:
+            queryset = queryset.filter(name=car_name)
+        return queryset
+
+
+class CreateCar(generics.CreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Car.objects.all()
     serializer_class = CarSerializer
