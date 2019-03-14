70fd29847e1792b6f3c118d53817f2bfde470b36
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-14 14:24:43 +0100

    ch(add-car-model): add car model to app
    - add car model
    - rename folders

---
+++ cars/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Car(models.Model):
+    name = models.CharField(max_length=250,unique=True,null=False)
+    kind = models.CharField(max_length=250,null=False)
+    year_of_creation = models.DateTimeField(null=False)
+    milleage = models.IntegerField(null=True,default=0)
+    color = models.CharField(max_length=250,null=True)
+    car_maker = models.CharField(max_length=250,null=True)
+    is_available = models.BooleanField(null=False,default=False)
+    car_engine_no = models.CharField(max_length=250,null=True,unique=True)
+    deleted = models.BooleanField(null=True,default=False)
+    status = models.CharField(max_length=250,null=False,default='new')
+    price = models.FloatField(null=False,default=0.00)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.name
