4a76ff8a15958a53f6b43c1172f1326e3dc9c774
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-15 10:16:29 +0100

    ft(authentication): allow logged in users to routes
    - authenticate routes to logged in users

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt import views as jwt_views
+
 from .views import CreateCar, RetrieveUpdateDestroyCar, ListCar
 
 app_name = 'cars'
@@ -8,7 +10,9 @@ app_name = 'cars'
 urlpatterns = {
     url(r'^cars/create/$', CreateCar.as_view(), name="create"),
     url(r'^cars/(?P<pk>[0-9]+)/$', RetrieveUpdateDestroyCar.as_view(), name="details"),
-    url(r'^cars\.??(?:&?[^=&]*=[^=&]*)*', ListCar.as_view(), name="list")
+    url(r'^cars\.??(?:&?[^=&]*=[^=&]*)*', ListCar.as_view(), name="list"),
+    url('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
