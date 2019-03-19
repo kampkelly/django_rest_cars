2814177ca542465bc5e48d30503cf0819193a01a
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-19 10:22:42 +0100

    bg(fix-urls): fix regex urls
    fix regex urls

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,11 @@ from .views import CreateCar, RetrieveUpdateDestroyCar, ListCar
 app_name = 'cars'
 
 urlpatterns = {
-    url(r'^cars/create/$', CreateCar.as_view(), name="create"),
-    url(r'^cars/(?P<pk>[0-9]+)/$', RetrieveUpdateDestroyCar.as_view(), name="details"),
-    url(r'^cars\.??(?:&?[^=&]*=[^=&]*)*', ListCar.as_view(), name="list"),
+    url(r'^cars$', CreateCar.as_view(), name="create"),
+    url(r'^cars/(\w\D)??(?:&?[^=&]*=[^=&]*)*', ListCar.as_view(), name="list"),
+    url(r'^cars/(?P<pk>[0-9]+)$', RetrieveUpdateDestroyCar.as_view(), name="details"),
     url('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    url('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
